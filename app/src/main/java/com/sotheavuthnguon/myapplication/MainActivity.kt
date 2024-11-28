21f5d92cf8d9a42009d576696c66992c6fb47438
Sotheavuth Nguon <sotheavuth.nguon@canadiabank.com.kh>
2024-11-28 13:08:56 +0700

    First commit

---
+++ app/src/main/java/com/sotheavuthnguon/myapplication/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.sotheavuthnguon.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.sotheavuthnguon.sotheavuthlibrary.HelloWorldPrinter
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        HelloWorldPrinter.printHelloWorld()
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
